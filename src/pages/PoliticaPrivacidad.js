5f4da726f955038d7f152fea75a2d04b8f30ba74
anthony <tony.anthony@hotmail.es>
2023-05-31 18:16:47 -0500

    Politica Privacidad 3

--- src/pages/PoliticaPrivacidad.js
+++ src/pages/PoliticaPrivacidad.js
@@ -14,7 +14,7 @@ function PoliticaPrivacidad() {
             <NavBarTop />
             <div className="privaty">
                 <div className="texto">
-                    <h1>Politica de uso de datos</h1>
+                    <h1>Política de uso de datos</h1>
                     <br/>
                     <br/>
                     <p>Mediante esta declaración, expreso mi consentimiento voluntario e informado y autorizó a Vehicentro y sus marcas, junto con sus empresas filiales y relacionadas en general, para el almacenamiento, acceso, uso y verificación de los datos personales que he proporcionado de manera voluntaria. Esta autorización abarca el uso de dichos datos para fines estadísticos, comerciales, informativos, seguimiento de productos, marketing, notificaciones y contacto con el cliente en campañas de seguridad, satisfacción, marketing relacional y otros similares, así como para la oferta de planes comerciales y financieros destinados a la adquisición de productos y/o servicios de las marcas comercializadas por Vehicentro. Se garantiza el cumplimiento de la privacidad y confidencialidad establecidas por las leyes ecuatorianas. Declaro que la información proporcionada es clara, suficiente y veraz.
