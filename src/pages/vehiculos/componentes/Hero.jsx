2619addc44be57ec8737d715c47b039734b6b071
anthony <tony.anthony@hotmail.es>
2023-06-19 10:46:04 -0500

    Heross

--- src/pages/vehiculos/componentes/Hero.jsx
+++ src/pages/vehiculos/componentes/Hero.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "../../../vehiculos.css";
+import env from '../../../config';
+
+let urlMedia = env.url +"/vehiculos/"
 
 function Hero() {
+
   return (
     <>
     <div className='vehiculosBox'>
-        <img src="" alt="" />
+        <img src={urlMedia + "fondoVehiculo.webp"} alt="" />
     </div>
+
     </>
   )
 }
