ecc26d1928beb4ce81bd231561a13ec288cdb451
DavidConstante <omileconstante@gmail.com>
2023-06-20 13:14:02 -0500

    Carousel v0.0.1

---
+++ src/pages/vehiculos/componentes/Interior.js
@@ -0,0 +1,32 @@
+import env from '../../../config'
+import MultiItemCarousel from './MultiItemCarousel'
+import './styles/interior.css'
+
+
+
+
+const Interior = () => {
+  const urlMedia = env.url + "vehiculos/"
+
+
+
+  return (
+    <div className='interior'>
+
+      <h3 className='title'> Upscale interior</h3>
+
+
+
+      <MultiItemCarousel />
+
+
+
+
+      <img src={urlMedia + "imagenInterior.webp"} alt="" />
+
+    </div>
+
+  )
+}
+
+export default Interior
---
+++ src/pages/vehiculos/componentes/MultiItemCarousel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MultiItemCarousel = () => {
+  return (
+    <div>
+
+      <div>
+        <p>Pantalla multimedia</p>
+        <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default MultiItemCarousel
