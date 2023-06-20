d92e3f6fb0423763392d1d4bb4c967cb37ba2651
DavidConstante <omileconstante@gmail.com>
2023-06-20 15:18:14 -0500

    Carousel v0.0.2

--- src/pages/vehiculos/componentes/MultiItemCarousel.jsx
+++ src/pages/vehiculos/componentes/MultiItemCarousel.jsx
@@ -2,27 +2,22 @@ import React from 'react'
 
 const MultiItemCarousel = () => {
   return (
-    <div className='flex'>
+    <div className='flex bg-red-200 w-11/12'>
 
-      <div className='w-[10rem]'>
+      <div className='w-[300px] bg-blue-300'>
         <p>Pantalla multimedia</p>
         <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
       </div>
 
-      <div className='w-[10rem]'>
+      <div className='w-[300px] bg-blue-300'>
         <p>Pantalla multimedia</p>
         <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
       </div>
 
-      <div className='w-[10rem]'>
-        <p>Pantalla multimedia</p>
-        <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
-      </div>
 
-      <div className='w-[10rem]'>
-        <p>Pantalla multimedia</p>
-        <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
-      </div>
+
+
+
 
     </div>
   )
