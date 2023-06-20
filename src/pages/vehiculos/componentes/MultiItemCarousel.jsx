22283421833ab2ddcf8d36679c61b12720207ba0
DavidConstante <omileconstante@gmail.com>
2023-06-20 13:23:03 -0500

    Install Tailwind

--- src/pages/vehiculos/componentes/MultiItemCarousel.jsx
+++ src/pages/vehiculos/componentes/MultiItemCarousel.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const MultiItemCarousel = () => {
   return (
-    <div>
+    <div className='flex'>
 
-      <div>
+      <div className='w-[10rem]'>
+        <p>Pantalla multimedia</p>
+        <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
+      </div>
+
+      <div className='w-[10rem]'>
+        <p>Pantalla multimedia</p>
+        <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
+      </div>
+
+      <div className='w-[10rem]'>
+        <p>Pantalla multimedia</p>
+        <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
+      </div>
+
+      <div className='w-[10rem]'>
         <p>Pantalla multimedia</p>
         <p>The available 12.3-inch Dual Panoramic Displays with navigation offer over a combined 24-inches of touchscreen for entertainment, convenience, and more.</p>
       </div>
