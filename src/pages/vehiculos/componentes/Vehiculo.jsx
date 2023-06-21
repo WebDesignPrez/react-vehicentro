6b4e419cc3000505ad9d9ab14479340aaa7ae1e5
anthony <tony.anthony@hotmail.es>
2023-06-21 13:49:18 -0500

    changes

--- src/pages/vehiculos/componentes/Vehiculo.jsx
+++ src/pages/vehiculos/componentes/Vehiculo.jsx
@@ -1,28 +1,32 @@
-import env from './../../../config'
-import './styles/vehiculo.css'
+import React, { useState } from 'react';
+import env from './../../../config';
+import './styles/vehiculo.css';
 
 const Vehiculo = () => {
+  const urlMedia = env.url + 'vehiculos/';
 
-  const urlMedia = env.url + "vehiculos/"
+  const [selectedImage, setSelectedImage] = useState(urlMedia + 'blanco.png');
+
+  const handleButtonClick = (newImage) => {
+    setSelectedImage(urlMedia + newImage + '.png');
+  };
+
   return (
     <div className='vehiculos'>
-
-      <img src={urlMedia + 'blancoCuadrado-.jpg'} alt="" />
+      <div className='contenedorVehiculos'>
+        <img className='carImage' src={selectedImage} alt="" />
+      </div>
 
       <div className='description'>
-        <h4>Stand up to the elements.</h4>
-        <p>Having been tested in some of the coldest climates on the planet, the EV6 is designed to handle the elements. Even in challenging weather, the EV6 can help keep you comfortable and in control with no sacrifice to speed. Take on more with these available features:</p>
-        <ul>
-          <li> A Heat Pump, which helps preserve all-electric range in cold weather driving conditions1 </li>
-          <li>Standard heated front seats and available heated rear seats to help keep you and your passengers comfortable2</li>
-          <li>Heated steering wheel designed to warm your hands on cold days</li>
-          <li>Dual Motor e-All Wheel Drive3</li>
-          <li>Standard remote start with cabin preconditioning4</li>
-        </ul>
-
+      <div className='buttons'>
+  <button className='blanco' onClick={() => handleButtonClick('blanco')}></button>
+  <button className='bluesky' onClick={() => handleButtonClick('bluesky')}></button>
+  <button className='conchevino' onClick={() => handleButtonClick('conchevino')}></button>
+  <button className='gris' onClick={() => handleButtonClick('gris')}></button>
+</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Vehiculo
+export default Vehiculo;
