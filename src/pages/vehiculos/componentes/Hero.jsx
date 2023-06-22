7b2f77b316518d2ac47ef68651b44869cc2748d7
DavidConstante <omileconstante@gmail.com>
2023-06-22 16:44:34 -0500

    Format

--- src/pages/vehiculos/componentes/Hero.jsx
+++ src/pages/vehiculos/componentes/Hero.jsx
@@ -2,23 +2,25 @@ import React from 'react'
 import "../../vehiculos/componentes/styles/hero.css";
 import env from '../../../config';
 
-let urlMedia = env.url +"/vehiculos/"
+let urlMedia = env.url + "/vehiculos/"
 
 function Hero() {
 
   return (
     <>
-    <div className='vehiculosBox'>
-          <div className='imagenesLogos'>
-            <img src={urlMedia + "iconosinotruk.webp"}></img>
-            <img className='u70' src={urlMedia + "U70prologo.webp"}></img>
-            <div className='headHero'>
-         <p className='tituloHeadHero mas'>Desde</p>
-         <p className='tituloHeadHero'>$20.990</p>
-       </div>
+      <div className='vehiculosBox'>
+
+        <div className='imagenesLogos'>
+          <img src={urlMedia + "iconosinotruk.webp"}></img>
+          <img className='u70' src={urlMedia + "U70prologo.webp"}></img>
+
+          <div className='headHero'>
+            <p className='tituloHeadHero mas'>Desde</p>
+            <p className='tituloHeadHero'>$20.990</p>
+          </div>
         </div>
-        
-    </div>
+
+      </div>
 
     </>
   )
