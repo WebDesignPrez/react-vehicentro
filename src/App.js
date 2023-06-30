73eed45370b7057ab08228fa70baa24ee2dda127
DavidConstante <omileconstante@gmail.com>
2023-06-30 15:42:14 -0500

    Changes in U70 Page

--- src/App.js
+++ src/App.js
@@ -90,8 +90,8 @@ import Minicargadoras_landing_extra from "./pages/Minicargadoras-landing-extra"
 import Minicargadoras_landing_primicias from "./pages/Minicargadoras-landing-primicias"
 import Minicargadoras_landing_revista_motors from "./pages/Minicargadoras-landing-revista-motors"
 import Minicargadoras_landing_universo from "./pages/Minicargadoras-landing-universo"
-import VehiculosPage from "./pages/vehiculos/VehiculosPage"
-import U70Autos from "./pages/vehiculos/U70Autos"
+import U70Pro from "./pages/vehiculos/U70Pro"
+import U70 from "./pages/vehiculos/U70"
 
 function App() {
 
@@ -135,10 +135,10 @@ function App() {
           <Route path="/serie/especiales" element={<Vehiculos_especiales />} />
           <Route path="/serie/especiales" element={<Vehiculos_especiales />} />
           <Route path="/proximamente-hidromek" element={<Proximamente_hidromek />} />
-          <Route path="/rendimiento/t5g" element={<Rendimiento/>} />
-          <Route path="/rendimiento/100" element={<Rendimiento100/>} />
-          <Route path="/rendimiento/c7h" element={<Rendimientoc7h/>} />
-          <Route path="/rendimiento/t7h" element={<Rendimientot7h/>} />
+          <Route path="/rendimiento/t5g" element={<Rendimiento />} />
+          <Route path="/rendimiento/100" element={<Rendimiento100 />} />
+          <Route path="/rendimiento/c7h" element={<Rendimientoc7h />} />
+          <Route path="/rendimiento/t7h" element={<Rendimientot7h />} />
           <Route path="/landing/patiotuerca" element={<Patiotuerca />} />
           <Route path="/landing/el-universo" element={<El_universo />} />
           <Route path="/landing/el-extra" element={<El_extra />} />
@@ -192,9 +192,10 @@ function App() {
           <Route path="/landing/revista-motors/volqueta/:id" element={<Volqueta_landing_revista_motors />} />
           <Route path="/landing/revista-motors/mixer/:id" element={<Mixer_landing_revista_motors />} />
           <Route path="/landing/revista-motors/minicargadoras/:id" element={<Minicargadoras_landing_revista_motors />} />
-          <Route path="/politicaprivacidad" element={<PoliticaPrivacidad/>} />
-          <Route path="/sinotruk/autos/u70pro" element={<VehiculosPage/>} />
-          <Route path="/sinotruk/autos/u70" element={<U70Autos/>} />
+          <Route path="/politicaprivacidad" element={<PoliticaPrivacidad />} />
+          <Route path="/sinotruk/autos/u70pro" element={<U70Pro />} />
+          <Route path="/sinotruk/autos/u70" element={<U70 />} />
+
 
         </Routes>
       </main>
--- src/pages/vehiculos/componentes/Hero.jsx
+++ src/pages/vehiculos/componentes/Hero.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import "../../vehiculos/componentes/styles/hero.css";
 import env from '../../../config';
 
-let urlMedia = env.url + "/vehiculos/"
 
-function Hero() {
+function Hero({ path }) {
+  let urlMedia = env.url + path
+
 
   return (
-    <div className='hero'>
-      <img src="https://vehicentro.com/images/vehiculos/herou70pro.png" alt="U70PRO" />
+    <div className='hero '>
+      <img src={urlMedia + "hero.png"} alt="U70" />
     </div>
   )
 }
