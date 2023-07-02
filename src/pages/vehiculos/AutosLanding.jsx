93dbabbd6f11411e3e4b08e430a88a562674deb5
anthony <tony.anthony@hotmail.es>
2023-07-01 23:26:59 -0500

    Format

--- src/pages/vehiculos/AutosLanding.jsx
+++ src/pages/vehiculos/AutosLanding.jsx
@@ -5,22 +5,17 @@ import { Helmet } from "react-helmet";
 import { NavLink } from "react-router-dom";
 import FormContact from "../../components/FormContact";
 import env from '../../config';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Detalles from './componentes/Detalles'
+import { Link } from 'react-router-dom';
 
 let series
 let nombreCamion
 let camionSerie
 let urlMedia = env.url
-let audio_motor = "../images/10-toneladas/motor.mp3"
+
 
 function Excavadora() {
-
-    useEffect(() => {
-        const motorPlayer = document.getElementById('audioMotor')
-        motorPlayer.src = audio_motor
-
-    }, []);
-
     return (
         <>
             <Helmet>
@@ -33,8 +28,9 @@ function Excavadora() {
             <Content1 />
             <Content2 />
             <Content3 />
-           {/*  <Content4 /> */}
+            {/*  <Content4 /> */}
             <Content5 />
+            <Detalles />
             <Footer />
         </>
     )
@@ -43,19 +39,9 @@ function Excavadora() {
 function Content1() {
     return (
         <>
-            <div className="boxesIni posRelative content2 fondonegro">
-                <div className="boxRight">
-                    <div className="boxLeft c7h">
-                        <img src={urlMedia + "swe210/excavadora.webp"} width="1400" height="1000" className="slideMain" alt="Punto de Venta" />
-                    </div>
-                </div>
-                <div className="boxLeft playMotor contenedorTextoSobrepuesto">
-                    <img src={urlMedia + "swe210/motor.webp"} width="1400" height="1000" className="slideMain" alt="Punto de Venta" />
-                    <div className="boxPlayerMotor ">
-                        <audio id="audioMotor" controls type="audio/mpeg"></audio>
-                    </div>
-                </div>
-            </div>
+            < div className="block2 noResponsive" >
+                <video preload="none" width="1920" height="1076" controls autoPlay={true} playsInline={true} muted loop src="https://www.vehicentro.com/cotizacion/images/u70pro/u70pro.mp4" className="videoWidth" />
+            </div >
         </>
     )
 }
@@ -73,33 +59,51 @@ function Content2() {
     }
     ]
 
+    const urlMedia = "https://www.vehicentro.com/images/vehiculos/";
+
+
+    const [selectedImage, setSelectedImage] = useState(urlMedia + 'blanco.webp');
+  
+    const handleButtonClick = (newImage) => {
+      setSelectedImage(urlMedia + newImage + '.webp');
+    };
+
+   
+
     return (
         <>
-            <div className="boxesIni posRelative content2 fondonegro">
+            <div className="boxesIni posRelative content2 fondonegro ">
 
                 <div className="boxRight nomSeries">
 
-                    <div className="serieTitle serieBox">
-                        <p className="serieTitle1">SUV</p>
-                        <p>SINOTRUK</p>
-                    </div>
+                    <img className="sinoLogo" src="https://www.vehicentro.com/images/sinotruk.webp"/>
 
-                    <div className="serieList serieBox">
-                        {series.map((item, index) => (
-                            <NavLink className="serieContainer" to={item.url}>
-                                <p className="nombreSerie">
-                                    {item.serie}
-                                </p>
-                                <span className="modeloSerie">
-                                    {item.modelo}
-                                </span>
-                            </NavLink>
-                        ))}
-                    </div>
+                    <div className="serieSinotruk">
+      <Link to="../sinotruk/autos/u70">
+        <img src="https://www.vehicentro.com/images/u70Logo2.webp" alt="" />
+      </Link>
+      <Link to="../sinotruk/autos/u70pro">
+        <img src="https://www.vehicentro.com/images/u70proLogo2.webp" alt="" />
+      </Link>
+    </div>
 
                 </div>
-                <div className="boxLeft playMotor contenedorTextoSobrepuesto">
-                    <img src={urlMedia + "swe365e/vehicentroExcavadoraEcuador.webp"} width="1400" height="1000" className="slideMain" alt="Punto de Venta" />
+                <div className='vehiculos fondoautos'>
+
+                    <img className='carImage' src={selectedImage} alt="U70PRO" />
+
+
+                    <div className='description'>
+                        <p>COLORES</p>
+                        <div className='buttons'>
+                            <button className='blanco' onClick={() => handleButtonClick('blanco')}></button>
+                            <button className='bluesky' onClick={() => handleButtonClick('bluesky')}></button>
+                            <button className='conchevino' onClick={() => handleButtonClick('conchevino')}></button>
+                            <button className='gris' onClick={() => handleButtonClick('gris')}></button>
+                            <button className='negro' onClick={() => handleButtonClick('negro')}></button>
+                            <button className='plateado' onClick={() => handleButtonClick('plateado')}></button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
@@ -115,15 +119,15 @@ function Content3() {
                 <div className="marcasbg textoImagenesProducto" >
 
                     <div className="boxRightHeader  half icon_center series" >
-                        <div className="banner"><img src={urlMedia + "swe210/jpn.png"} /></div>
+                        <div className="banner images autos1"><img src={urlMedia + "bosch.png"} /></div>
                     </div>
 
                     <div className="boxRightHeader  half text_icon_center series" >
-                        <div className="banner"><img src={urlMedia + "swe210/isuzu.webp"} /></div>
+                        <div className="banner images autos2"><img src={urlMedia + "delphi.png"} /></div>
                     </div>
 
                     <div className="boxRightHeader  half icon_right series">
-                        <div className="banner"><img src={urlMedia + "swe210/5-anos-de-garantia.webp"} /></div>
+                        <div className="banner images autos3"><img src={urlMedia + "magnettiAzul.png"} /></div>
                     </div>
 
                 </div>
@@ -138,13 +142,13 @@ function Content3() {
 function Content5() {
     return (
         <div className="boxesIni posRelative">
-        <div className="centrado">
-            <img src={urlMedia + "vehiculos/imgForm.png"} width="1400" height="1000" className="slideMain" alt="Punto de Venta" />
+            <div className="centrado">
+                <img src={urlMedia + "vehiculos/imgForm.png"} width="1400" height="1000" className="slideMain" alt="Punto de Venta" />
+            </div>
+            <div>
+                <FormContact url="https://bdc.vehicentro.com:9443/ords/ws_vehicentro/api/conexiones/wordpress/WEBVHSERIEMINICAR" camion={nombreCamion} serie="autos" camionSerie={camionSerie} />
+            </div>
         </div>
-        <div>
-            <FormContact url="https://bdc.vehicentro.com:9443/ords/ws_vehicentro/api/conexiones/wordpress/WEBVHSERIEMINICAR" camion={nombreCamion} serie="autos" camionSerie={camionSerie} />
-        </div>
-    </div>
     )
 }
 
