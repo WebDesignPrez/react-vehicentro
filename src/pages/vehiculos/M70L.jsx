202dd4b078ceb881a2593dfed0945203154fb876
anthony <tony.anthony@hotmail.es>
2023-07-13 20:14:10 -0500

    Fix: Change Photo 19ton 3/4

---
+++ src/pages/vehiculos/M70L.jsx
@@ -0,0 +1,455 @@
+import { useEffect, useState, React  } from "react";
+import NavBarTop from '../../NavBarTop'
+import './componentes/styles/galeria.css'
+import Carousel from 'better-react-carousel'
+import CompraMenu from './componentes/AmenidadesU70Pro'
+import Footer from '../../components/Footer'
+import InteriorU70Pro from './componentes/InteriorU70Pro'
+import './componentes/styles/detalles.css'
+
+import { Helmet } from "react-helmet";
+
+import './componentes/styles/vehiculo.css';
+
+import env from '../../config';
+
+import FormContact from '../../../src/components/FormContact';
+
+
+
+
+function M70L() {
+
+  
+
+  return (
+    <>
+
+      <Helmet>
+        <title> M70L - Vehicentro</title>
+        <meta name="description" content={'Venta del suv mas largo de Ecuador'} />
+        <meta name="keywords" content="autos, SUV, venta de SUV, suv en ecuador"></meta>
+        <meta name="author" content="Celimo Constante, Anthony Perez, Prez"/>
+        <meta name="robots" content="index, follow"></meta>
+      </Helmet>
+
+
+<NavBarTop />
+
+      <Hero/>
+
+      {/* Characteristics */}
+      <CompraMenu />
+
+      {/* Colors of car (Tecnologies) */}
+      <Vehiculo/>
+
+      {/* Two pages of three images */}
+      <Galeria/>
+
+      {/* Component for interior images */}
+      <InteriorU70Pro />
+
+      <FormularioU70ProMedios />
+
+      {/* Security */}
+      {/* <Detalles2 /> */}
+
+      <Footer />
+
+
+    </>
+  )
+}
+
+function Hero() {
+
+  
+  
+    return (
+        < div className="block2" >
+        <video playsInline preload width="1920" height="1076" controls autoPlay muted loop src="https://www.vehicentro.com/images/furgonetas/keitonVideo.mp4" className="videoWidth" />
+        </div >
+    )
+  }
+  
+  function AmenidadesU70Pro() {
+    return (
+      <>
+        <div className='compraBox'>
+  
+          <div className='option'>
+            <div className='uno'>
+              <div className='izquierda'>
+                <img src= "icono1.png" alt='Asiento de Pasajeros Auto U70PRO' />
+                <p>7 Pasajeros</p>
+              </div>
+              <div className='derecha'>
+                <img src= "icono2.png" alt='Icono Motor U70PRO 154HP' />
+                <p>154 HP</p>
+              </div>
+              <div className='izquierda'>
+                <img src= "icono3.png" alt='Rin 18 pulgadas' />
+                <p>18 Pulgadas</p>
+              </div>
+              <div className='derecha'>
+                <img src= "icono4.png" alt='Frenos de disco U70PRO' />
+                <p>Frenos de Disco</p>
+              </div>
+            </div>
+            <div className='dos'>
+              <div className='izquierda2'>
+                <img className='suvCarro' src= "icono5.png" alt='Largo del auto U70PRO' />
+                <p>4825 mm</p>
+              </div>
+              <div className='derecha2'>
+                <img src= "icono6.png" alt='Techo panorámico U70PRO' />
+                <p>Techo Panorámico</p>
+              </div>
+              <div className='derecha2'>
+                <img src= "icono7.png" alt='Gasolina Extra' />
+                <p>Gasolina Extra</p>
+              </div>
+            </div>
+          </div>
+          <div className='image'>
+            <video preload="none" autoPlay={true} width="930" height="1080" playsInline={true} loop muted id="myVideo" src= "animacionAsientos.mp4" className="videoWidth produ" />
+          </div>
+        </div>
+        <div className='compraBox responsive'>
+          <div className='option'>
+            <div className='uno'>
+              <div className='izquierda'>
+                <img src="icono1.png" alt='Asiento de Pasajeros Auto U70PRO' />
+                <p>7 Pasajeros</p>
+              </div>
+              <div className='derecha'>
+                <img src= "icono2.png" alt='Icono Motor U70PRO 154HP' />
+                <p>154 HP</p>
+              </div>
+            </div>
+            <div className='uno'>
+              <div className='izquierda'>
+                <img src= "icono3.png" alt='Rin 18 pulgadas' />
+                <p>18 Pulgadas</p>
+              </div>
+              <div className='derecha'>
+                <img src= "icono4.png" alt='Frenos de disco U70PRO' />
+                <p>Frenos de Disco</p>
+              </div>
+            </div>
+            <div className='dos'>
+              <div className='izquierda2'>
+                <img src= "icono5.png" alt='Largo del auto U70PRO' />
+                <p>4825 mm</p>
+              </div>
+              <div className='derecha2'>
+                <img src= "icono6.png" alt='Techo panorámico U70PRO' />
+                <p>Techo Panorámico</p>
+              </div>
+            </div>
+            <div className='dos'>
+              <div className='derecha2'>
+                <img src="icono7.png" alt='Gasolina Extra' />
+                <p>Gasolina Extra</p>
+              </div>
+              <div className='derecha2'>
+              </div>
+            </div>
+          </div>
+  
+  
+  
+  
+          <div className='image'>
+            <img src= "emocion1.webp" />
+          </div>
+  
+  
+        </div>
+      </>
+    )
+  }
+  
+
+  const Vehiculo = () => {
+    
+  
+    return (
+      <div className='todo'>
+        <div className='vehiculos'>
+  
+          <img className='carImage' src="" alt="U70PRO" />
+  
+  
+          <div className='description'>
+            <p>COLORES</p>
+            <div className='buttons'>
+             
+            </div>
+          </div>
+        </div>
+  
+        <div className='textoVehiculos'>
+          <img src='tecnologia.jpg' alt='Suv colores' />
+        </div>
+  
+      </div>
+  
+    );
+  };
+
+
+
+
+const Galeria = () => {
+
+
+
+
+
+  return (
+    <>
+      <div className='galeria'>
+        <h3 className='title'>Exterior</h3>
+      </div>
+
+      {
+        
+          <div className='carrousel'>
+            <Carousel
+              cols={3}
+              rows={1}
+              gap={30}
+              loop
+            >
+              <Carousel.Item>
+                <img width="100%" src="" alt="Faro delantero U70PRO" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width="100%" src="" alt="Direccionales U70PRO" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width="100%" src="" alt="Faro trasero U70PRO" />
+              </Carousel.Item>
+
+              <Carousel.Item>
+                <img width="100%" src="" alt="Rines de 18 pulgadasU70PRO" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width="100%" src="" alt="Techo Panoramico U70PRO" />
+              </Carousel.Item>
+              <Carousel.Item>
+                <img width="100%" src="" alt="Apertura sin llave U70PRO" />
+              </Carousel.Item>
+
+
+            </Carousel>
+          </div>
+      }
+
+
+
+    </>
+  )
+}
+
+
+
+
+const FormularioU70ProMedios = () => {
+
+
+  
+
+
+
+
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Limpieza del evento al desmontar el componente
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const [modalFormIsOpen, setFormIsOpen] = useState(false);
+
+  useEffect(() => {
+    const keyDownHandler = event => {
+      if (event.key === 'Escape') {
+        event.preventDefault();
+        closeFormContact()
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
+
+
+  function openFormContact() {
+    setFormIsOpen(true)
+  }
+
+  function closeFormContact() {
+    setFormIsOpen(false)
+  }
+
+  
+      let nombreCamion = "U70PRO"
+      let camionSerie = "U70PRO"
+      let nombreSerie = "autos"
+      let bdc = "https://bdc.vehicentro.com:9443/ords/ws_vehicentro/api/conexiones/wordpress/WEBAUTU70P"
+
+
+
+
+  return (
+    <div className="boxesIni posRelative">
+      <div className="centrado">
+        <img src="" width="1400" height="1000" className="slideMain" alt="SUV U70" />
+      </div>
+      <div>
+        <FormContact url={`https://bdc.vehicentro.com:9443/ords/ws_vehicentro/api/conexiones/wordpress/`} camion={'U70PRO'} serie={'autos'} camionSerie={'U70PRO'} />
+      </div>
+    </div>
+
+  );
+};
+
+
+
+
+const Detalles2 = () => {
+
+
+
+  return (
+    <>
+      <div className='page'>
+
+
+        <div className='imagenes'>
+          <img src="" alt="" />
+        </div>
+        <div className='seguridadBoxes'>
+          <h3 className='title'>Sistemas de Seguridad</h3>
+          <div className='arriba'>
+            <div className='centrado'>
+              <img src= "seguridad1.png"alt='2 Airbags' />
+              <p>2 Airbags</p>
+            </div>
+            <div className='centrado'>
+              <img src="seguridad2.png" alt='Sistema antibloqueo de frenos' />
+              <p>Frenos ABS</p>
+            </div>
+            <div className='centrado'>
+              <img src="seguridad3.png" alt='Distribución electrónica fuerza de frenado' />
+              <p>EBD Distribución electrónica fuerza de frenado</p>
+            </div>
+          </div>
+
+          <div className='abajo'>
+            <div className='centrado'>
+              <img src="seguridad4.png" alt='Asistente de arranque en pendiente' />
+              <p>HAC Asistente de arranque en pendiente</p>
+            </div>
+            <div className='centrado'>
+              <img src=  "seguridad5.png" alt='Programa electrónico de estabilidad' />
+              <p>EPS Programa electrónico de estabilidad</p>
+            </div>
+            <div className='centrado'>
+              <img src=  "seguridad6.png" alt='Control electrónico de tracción' />
+              <p>TCS Control electrónico de tracción</p>
+            </div>
+          </div>
+          <div className='abajo'>
+            <div className='centrado'>
+              <img src= "seguridad7.png" alt='Control de asistencia para descenso en pendiente' />
+              <p>DAC Control de asistencia para descenso en pendiente</p>
+            </div>
+            <div className='centrado'>
+              <img src=  "seguridad8.png" alt='Sistema antivuelco' />
+              <p>ARP Sistema antivuelco</p>
+            </div>
+            <div className='centrado'>
+              <img src= "seguridad9.png" alt='Freno de estacionamiento electrónico' />
+              <p>EPB Freno de estacionamiento electrónico</p>
+            </div>
+          </div>
+        </div>
+
+       {/*  Celular */}
+       <div className='celular'>
+          <h3 className='title'>Sistemas de Seguridad</h3>
+          <div className='arribaCelular'>
+            <div className='centrado'>
+              <img src= "seguridad1.png" alt='2 Airbags' />
+              <p>2 Airbags</p>
+            </div>
+            <div className='centrado'>
+              <img src= "seguridad2.png" alt='Sistema antibloqueo de frenos' />
+              <p>Frenos ABS</p>
+            </div>
+           
+          </div>
+
+          <div className='abajoCelular'>
+          <div className='centrado'>
+              <img src= "seguridad3.png" alt='Distribución electrónica fuerza de frenado' />
+              <p>EBD Distribución electrónica fuerza de frenado</p>
+            </div>
+            <div className='centrado'>
+              <img src= "seguridad4.png" alt='Asistente de arranque en pendiente' />
+              <p>HAC Asistente de arranque en pendiente</p>
+            </div>
+            
+          </div>
+          <div className='abajoCelular'>
+            <div className='centrado'>
+              <img src= "seguridad5.png" alt='Programa electrónico de estabilidad' />
+              <p>EPS Programa electrónico de estabilidad</p>
+            </div>
+            <div className='centrado'>
+              <img src= "seguridad6.png" alt='Control electrónico de tracción' />
+              <p>TCS Control electrónico de tracción</p>
+            </div>
+          </div>
+          <div className='abajoCelular'>
+            <div className='centrado'>
+              <img src="seguridad7.png" alt='Control de asistencia para descenso en pendiente' />
+              <p>DAC Control de asistencia para descenso en pendiente</p>
+            </div>
+            <div className='centrado'>
+              <img src= "seguridad8.png" alt='Sistema antivuelco' />
+              <p>ARP Sistema antivuelco</p>
+            </div>
+          </div>
+          <div className='abajoCelular ultimo'>
+            <div className='centrado'>
+              <img src= "seguridad9.png" alt='Freno de estacionamiento electrónico' />
+              <p>EPB Freno de estacionamiento electrónico</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+
+
+export default M70L
