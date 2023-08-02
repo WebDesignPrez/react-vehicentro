8bdc3006597166bca0a54cc7b4141c4cc343b582
anthony <tony.anthony@hotmail.es>
2023-08-02 11:44:06 -0500

    FIX: Change new prices and quotas

--- src/precios.js
+++ src/precios.js
@@ -2,67 +2,67 @@ let precios = {
 
 
     tres_cinco: " $23,990 ",
-    tres_cinco_cuota: " $413.62 ",
+    tres_cinco_cuota: " $375,00 ",
 
     tres_seis: " $23,990 ",
-    tres_seis_cuota: " $413.62 ",
+    tres_seis_cuota: " $375.00 ",
 
     cinco: " $27,990 ",
-    cinco_cuota: " $477.00 ",
+    cinco_cuota: " $428.00 ",
 
-    seis: " $30,890 ",
-    seis_cuota: " $522.00 ",
+    seis: " $31,890 ",
+    seis_cuota: " $494.00 ",
 
     ocho: " $42,990 ",
-    ocho_cuota: " $715.00 ",
+    ocho_cuota: " $663.00 ",
 
     nueve: " $49,990 ",
-    nueve_cuota: " $826.00 ",
+    nueve_cuota: " $768.00 ",
 
     diez: " $53,890 ",
-    diez_cuota: " $887.00 ",
+    diez_cuota: " $827.00 ",
 
     doce: " $57,990 ",
-    doce_cuota: " $953.00 ",
+    doce_cuota: " $889.00 ",
 
     trece: " $62,990 ",
-    trece_cuota: " $1,032.00 ",
+    trece_cuota: " $965.00 ",
 
     trece_330: " $67,990 ",
-    trece_330_cuota: " $1,112.00 ",
+    trece_330_cuota: " $1,040.00 ",
 
     d_ocho: " $74,990 ",
-    d_ocho_cuota: " $1,223.00 ",
+    d_ocho_cuota: " $1,147.00 ",
 
     d_nueve: " $80,990 ",
-    d_nueve_cuota: " $1,318.00 ",
+    d_nueve_cuota: " $1,237.00 ",
 
     veinte: " $96,990 ",
-    veinte_cuota: " $1,572.00 ",
+    veinte_cuota: " $1,477.00 ",
 
     v_cuatro: " $61,990 ",
-    v_cuatro_cuota: " $1,017.00 ",
+    v_cuatro_cuota: " $950.00 ",
 
     v_ocho: " $69,990 ",
-    v_ocho_cuota: " $1,144.00 ",
+    v_ocho_cuota: " $1,071.00 ",
 
     v_ocho_r: " $76,990 ",
-    v_ocho_r_cuota: " $1,255.00 ",
+    v_ocho_r_cuota: " $1,177.00 ",
 
     c_dos: " $81,990 ",
-    c_dos_cuota: " $1,334.00 ",
+    c_dos_cuota: " $1,252.00 ",
 
     c_cinco: " $89,990 ",
-    c_cinco_cuota: " $1,461.00 ",
+    c_cinco_cuota: " $1,373.00 ",
 
     c_ocho: " $102,990 ",
-    c_ocho_cuota: " $1,668.00 ",
+    c_ocho_cuota: " $1,571.00 ",
 
     c_ocho_c: " $106,990 ",
-    c_ocho_c_cuota: " $1,731.00 ",
+    c_ocho_c_cuota: " $1,631.00 ",
 
     e_210: " $161,280 ",
-    e_210_cuota: " $2,968.00 ",
+    e_210_cuota: " $2,977.00 ",
 
     e_365: " $257,600 ",
     e_365_cuota: " $4,710.00 ",
