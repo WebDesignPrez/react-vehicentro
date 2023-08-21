0186cf1ffe559bc5fd36ef8930e7294fd5bccfd3
mortyec <tony.anthony@hotmail.es>
2023-08-21 13:56:16 -0500

    FEATURE: Add privacity

--- src/pages/PoliticaPrivacidad.js
+++ src/pages/PoliticaPrivacidad.js
@@ -14,12 +14,96 @@ function PoliticaPrivacidad() {
             <NavBarTop />
             <div className="privaty">
                 <div className="texto">
-                    <h1>Política de uso de datos</h1>
-                    <br/>
-                    <br/>
-                    <p>Mediante esta declaración, expreso mi consentimiento voluntario e informado y autorizó a Vehicentro y sus marcas, junto con sus empresas filiales y relacionadas en general, para el almacenamiento, acceso, uso y verificación de los datos personales que he proporcionado de manera voluntaria. Esta autorización abarca el uso de dichos datos para fines estadísticos, comerciales, informativos, seguimiento de productos, marketing, notificaciones y contacto con el cliente en campañas de seguridad, satisfacción, marketing relacional y otros similares, así como para la oferta de planes comerciales y financieros destinados a la adquisición de productos y/o servicios de las marcas comercializadas por Vehicentro. Se garantiza el cumplimiento de la privacidad y confidencialidad establecidas por las leyes ecuatorianas. Declaro que la información proporcionada es clara, suficiente y veraz.
-</p>
+                    <h1>POLÍTICA DE PRIVACIDAD</h1>
+                    <br />
+                    <br />
+                    <p>Vehicentro Vehículos y Camiones Centro Sierra S.A, con RUC 1891724612001 domiciliada en Ambato, Av. Indoamérica Km 3 1/2 Vía a Quito, como RESPONSABLE del tratamiento de los datos personales del titular, obtenidos por medio de consentimiento verbal o escrito y conforme a lo dispuesto por la LEY ORGÁNICA DE PROTECCIÓN DE DATOS PERSONALES (LOPDP), pone a disposición la siguiente POLÍTICA DE PRIVACIDAD para informar al titular de los datos a ser tratados y protegidos por la organización.</p>
+                    <br />
+                    <p className="black">1.	FINALIDAD DEL TRATAMIENTO DE LOS DATOS PERSONALES</p>
+                    <br />
+                    <p>Vehicentro tratará los datos personales del titular con la siguiente finalidad:</p>
+                    <br />
+                    <p className="black">Clientes:</p>
+                    <br />
+                    <div className="espacioText">
+                        <p>•    Pedir y enviar cotizaciones, asistencia, informes técnicos o información general de nuestros productos o servicios.</p>
+                        <p>•	Hacer pedidos de productos o servicios o registrarse en nuestros portales de mensajería o RRSS.</p>
+                        <p>•	Participar en encuestas, sorteos u otras actividades promocionales, sea que se lo haga en línea o en cualquier otro medio.</p>
+                        <p>•	Suscribirse a boletines informativos, mensajes promocionales por correo electrónico, mensajería, RRSS u otros materiales.</p>
+                        <p>•	Consultar e informar a las entidades de información crediticia (en los casos que se requiera).</p>
+                        <p>•	Facturación y reportería contable.</p>
+                        <p>•	Ponerse en contacto con nosotros.</p>
                     </div>
+                    <br />
+                    <p className="black">Colaboradores:</p>
+                    <br />
+                    <div className="espacioText">
+                        <p>•	Solicitudes de puestos de trabajo, enviar su currículum vítae o crear un perfil de solicitante de empleo.</p>
+                        <p>•	Gestión de nómina y beneficios del colaborador.</p>
+                        <p>•	Gestión de inducción y capacitaciones.</p>
+                        <p>•	Evaluación del desempeño y clima laboral.</p>
+                        <p>•	Elaboración de planes de carrera y promoción de los colaboradores.</p>
+                        <p>•	Seguridad y Salud ocupacional.</p>
+                        <p>•	Cumplir con cualquier obligación laboral de requerida por la LOPDP y el Código del Trabajo o cualquier otra normativa laboral vigente.</p>
+                    </div>
+                    <br />
+                    <p className="black">Proveedores:</p>
+                    <br />
+                    <div className="espacioText">
+                        <p>•	Gestión de preselección, verificación de referencias, selección y calificación de proveedores.</p>
+                        <p>•	Instrumentación de contratos.</p>
+                        <p>•	Soporte a incidencias por continuidad de negocio.</p>
+                        <p>•	Monitoreo de prestación del servicio y/o entrega del producto contratado.</p>
+                        <p>•	Cumplir con las obligaciones legales, contractuales y normativa aplicable a Vehicentro.</p>
+                    </div>
+                    <br />
+                    <p>Además, se utilizarán los datos de todos los titulares para cumplir con cualquier otro requisito dictaminado por la LOPDP.</p>
+                    <br />
+                    <p className="black">Datos Tratados:</p>
+                    <br />
+                    <p>Para Clientes, la información personal que pedimos puede incluir su dirección de correo electrónico, nombre y apellido, domicilio y número de teléfono.  </p>
+                    <p>Para procesar su solicitud de crédito o financiación, es posible que también se le pida acceder a su historial crediticio, cuentas bancarias, datos personales del cónyuge y/o garante y otros datos relevantes sensibles.</p>
+                    <p>Además, se podrá transferir los datos del titular a otro responsable para que sea tratado en relación con seguros, solicitudes de crédito y prospección de clientes.</p>
+                    <br />
+                    <p>Para Aspirantes a Colaboradores, tendrán que suministrar toda la información antes detallada, además de sus antecedentes de educación, experiencia y formación laboral. Por otro lado, en caso ser seleccionado para un puesto, pasando a ser Colaboradores en firme, para gestión de nómina y obligaciones legales, la empresa deberá acceder y tratar datos sensibles como: relaciones familiares, identidad de género, etnia, salud, datos biométricos y otros datos de intereses y preferencias personales.</p>
+                    <br />
+                    <p>Para Proveedores, la información puede incluir su dirección de correo electrónico, nombre y apellido, domicilio laboral y número de teléfono. Además, cualquier dato adicional que se necesite para calificar y seleccionar proveedores según corresponda por cada proceso.</p>
+                    <br/>
+                    <p className="black">2.	TRANSFERENCIA DE DATOS PERSONALES</p>
+                    <br/>
+                    <p>Vehicentro podrá compartir su información personal con terceros como lo son empresas filiales o socios comerciales debidamente autorizados:</p>
+                    <br/>
+                    <div className="espacioText">
+                       <p>•	Entidades de Software de la información</p>
+                       <p>•	Proveedores de Repuestos</p>
+                       <p>•	Proveedores de Seguros</p>
+                       <p>•	Entidades Crediticias</p>
+                    </div>
+                    <br/>
+                    <p>Vehicentro exigirá a los terceros a quienes se transfieran sus datos personales el cumplimiento de estándares adecuados de confidencialidad, protección y seguridad de la información.</p>
+                    <br/>
+                    <p className="black">3.	CONSENTIMIENTO, RECOPILACIÓN Y USO DE LOS DATOS.</p>
+                    <br/>
+                    <p>Vehicentro proporcionará al titular o interesado los recursos técnicos adecuados para que otorgue su consentimiento, el cual será recabado mediante la aceptación de manera verbal, física, digital u otros medios.</p>
+                    <br/>
+                    <p>En función de su interés legítimo, Vehicentro analizará las preferencias de los titulares o interesados para ofrecer productos y servicios que se consideran de su interés.</p>
+                    <br/>
+                    <br/>
+                    <p>En caso de que el titular de los datos desee ejercer sus derechos a la información, acceso, rectificación, actualización, eliminación, portabilidad o suspensión del tratamiento, puede comunicar su solicitud a los datos de contactos de la organización suministrados en el siguiente apartado.</p>
+                    <br/>
+                    <p>Los datos del titular serán conservados única y exclusivamente por el tiempo necesario para cumplir con las finalidades de tratamiento, con los tiempos legales previstos en las normas legales aplicables y en un máximo del tiempo necesario para la prescripción de las acciones legales. Luego de este período los datos se eliminarán o bloquearán conforme a los mecanismos, herramientas y estrategias implementados por Vehicentro. </p>
+                    <p>El titular podrá revocar su consentimiento para el uso y tratamiento de sus datos en cualquier momento, siempre que no exista algún otro requerimiento legal que lo impida.</p>
+                    <br/>
+                    <p className="black">4.	CONTACTOS DEL DELEGADO DE LA ORGANIZACIÓN</p>
+                    <br/>
+                    <p>El Delegado de Protección de Datos Personales es la figura encargada de supervisar el cumplimiento de la LOPDP a fin de garantizar la protección de los datos personales que el titular o interesado facilita a Vehicentro.</p>
+                    <p>En caso de requerir contactar con el Delegado de Vehicentro para ejercer cualquier derecho del titular o para el acompañamiento en la revocatoria o modificaciones del consentimiento lo puede realizar mediante el correo privacidad@vehicentro.com, o acercarse a Vehicentro Matriz, Av. Indoamérica Km 3 1/2 Vía a Quito o mediante contacto telefónico de atención al cliente + 03 2 994740 ext. 1017.</p>
+                    <br/>
+                    <p className="black">5.	CAMBIOS AL AVISO DE PRIVACIDAD</p>
+                    <br/>
+                    <p>La actualización al presente contenido será notificada a través del sitio web de Vehicentro: https://vehicentro.com/</p>
+                    <p>Última Versión: 18/08/2023</p>
+                </div>
             </div>
             <BannerContacto />
             <Footer />
