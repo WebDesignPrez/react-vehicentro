0ceea900b1f8fbc197537658be13f74594ee84de
mortyec <tony.anthony@hotmail.es>
2024-01-10 10:41:16 -0500

    update trucks price

--- src/precios.js
+++ src/precios.js
@@ -1,76 +1,76 @@
 let precios = {
 
-    dos_cinco: "$18,990",
-    dos_cinco_cuota:'',
+    dos_cinco: "$20,490",
+    dos_cinco_cuota: '330.00',
 
 
     tres_cinco: "$23,990",
-    tres_cinco_cuota: "$460.00",
+    tres_cinco_cuota: "$385.00",
 
     tres_seis: "$23,990",
-    tres_seis_cuota: "$460.00",
+    tres_seis_cuota: "$385.00",
 
     cinco: "$27,990",
-    cinco_cuota: "$463.00",
+    cinco_cuota: "$448.00",
 
     seis: "$31,890",
-    seis_cuota: "$524.00",
+    seis_cuota: "$509.00",
 
-    ocho: " $42,990 ",
-    ocho_cuota: " $708.00 ",
+    ocho: " $42,990",
+    ocho_cuota: " $684.00 ",
 
-    nueve: " $49,990 ",
-    nueve_cuota: " $817.00 ",
+    nueve: " $49,990",
+    nueve_cuota: " $794.00 ",
 
     diez: " $53,890 ",
-    diez_cuota: " $883.00 ",
+    diez_cuota: " $857.00 ",
 
     doce: " $57,990 ",
-    doce_cuota: " $946.00 ",
+    doce_cuota: " $920.00 ",
 
     trece: " $62,990 ",
-    trece_cuota: " $1,030.00 ",
+    trece_cuota: " $999.00 ",
 
     trece_330: " $67,990 ",
-    trece_330_cuota: " $1,106.00 ",
+    trece_330_cuota: " $1,078.00 ",
 
     d_ocho: " $74,990 ",
-    d_ocho_cuota: " $1,224.00 ",
+    d_ocho_cuota: " $1,188.00 ",
 
     d_nueve: " $80,990 ",
-    d_nueve_cuota: " $1,318.00 ",
+    d_nueve_cuota: " $1,282.00 ",
 
     veinte: " $96,990 ",
-    veinte_cuota: " $1,445.00 ",
+    veinte_cuota: " $1,534.00 ",
 
     veinte_catalinas: " $104,990 ",
     veinte_catalinas_cuota: " $1,699.00 ",
 
 
     v_cuatro: " $61,990 ",
-    v_cuatro_cuota: " $999.00 ",
+    v_cuatro_cuota: " $983.00 ",
 
     v_ocho: " $69,990 ",
-    v_ocho_cuota: " $1,138.00 ",
+    v_ocho_cuota: " $1,109.00 ",
 
     v_ocho_r: " $76,990 ",
-    v_ocho_r_cuota: " $1,256.00 ",
+    v_ocho_r_cuota: " $1,220.00 ",
 
-    v_ocho_340: "$59,900", 
+    v_ocho_340: "$59,900",
 
     c_dos: " $81,990 ",
-    c_dos_cuota: " $1,334.00 ",
+    c_dos_cuota: " $1,299.00 ",
 
     c_cinco: " $89,990 ",
-    c_cinco_cuota: " $1,437.00 ",
+    c_cinco_cuota: " $1,424.00 ",
 
     c_ocho: " $102,990 ",
-    c_ocho_cuota: " $1,673.00 ",
+    c_ocho_cuota: " $1,628.00 ",
 
     c_ocho_c: " $106,990 ",
-    c_ocho_c_cuota: " $1,743.00 ",
+    c_ocho_c_cuota: " $1,692.00 ",
 
-    e_210: " $161,280 ",
+    e_210: " $161,280",
     e_210_cuota: "",
 
     e_365: " $257,600 ",
