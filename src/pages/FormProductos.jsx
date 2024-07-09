1e1bd6972bf989176a3cb0795be669b6ac944917
mortyec <tony.anthony@hotmail.es>
2024-07-09 17:15:30 -0500

    Cotizar form

---
+++ src/pages/FormProductos.jsx
@@ -0,0 +1,24 @@
+import { Helmet } from "react-helmet"
+import BannerContacto from "../components/BannerContacto"
+import Footer from "../components/Footer"
+import NavBarTop from "../NavBarTop"
+import FormNewProductos from "../components/FormNewProductos"
+
+
+function FormProductos() {
+  return (
+    <>
+      <Helmet>
+        <title>Términos y Condiciones  | Vehicentro</title>
+        <meta name="description" content="Términos y Condiciones" />
+      </Helmet>
+      {/* <NavBarTop /> */}
+      <FormNewProductos />
+      {/* <BannerContacto /> */}
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default FormProductos
+
